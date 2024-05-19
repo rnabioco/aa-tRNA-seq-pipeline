76098b3c267314f866689b165d3bfdcb419ae8d9
Kent Riemondy <kent.riemondy@gmail.com>
2024-05-19 10:42:29 -0600

    update test data to test merging multiple samples

--- .test/make_test_data.sh
+++ .test/make_test_data.sh
@@ -7,6 +7,7 @@
 # get reference fasta
 cp /beevol/home/riemondy/Projects/AAtRNAseq/ref/yeast.tRNAnanoref.wsplicing.fa ./
 
+
 # get dorado model
 dorado download --model rna004_130bps_sup@v3.0.1
 
@@ -23,6 +24,7 @@ samtools view -q 30 -F 20 $ex2/20231211_S288C_ctrl_chemligonly.bwa.bam | cut -f 
 samtools view -q 30 -F 20 $ex2/20231211_S288C_ctrl_chemligonly.bwa.bam | cut -f 1 | tail -n 100  > ex2_read_ids_2.txt
 samtools view -q 30 -F 20 $ex2/20231211_S288C_ctrl_chemligonly.bwa.bam | cut -f 1 | head -n 200 | tail -n 10   > ex2_read_ids_3.txt
 
+rm -rf sample1/ sample2/ sample2_1/
 od=sample1/pod5_pass
 rm -rf $od
 mkdir -p $od
@@ -59,8 +61,13 @@ od=sample2/fast5_fail
 mkdir -p $od
 pod5 convert to_fast5 -f -o $od sample2/pod5_fail/*.pod5
 
-dorado basecaller --emit-moves -v rna004_130bps_sup@v3.0.1 sample1/pod5_pass/*.pod5 > sample1/sample1.unmapped.bam
-dorado basecaller --emit-moves -v rna004_130bps_sup@v3.0.1 sample2/pod5_pass/*.pod5 > sample2/sample2.unmapped.bam
+dorado basecaller --emit-moves -v -r rna004_130bps_sup@v3.0.1 sample1/pod5_pass > sample1/sample1.unmapped.bam
+dorado basecaller --emit-moves -v -r rna004_130bps_sup@v3.0.1 sample2/pod5_pass > sample2/sample2.unmapped.bam
+
+# make another "sample2" dataset to test merging multiple runs
+# don't duplicate sample2 reads to avoid throwing an error when merging
+# pod5s
+cp -r sample1 sample2_1
 
 rm ex1*.txt ex2*.txt
 
