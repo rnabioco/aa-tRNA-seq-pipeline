0e425f01ae5307930d93f03f0a8d461d88bfdfe4
Jay Hesselberth <jay.hesselberth@gmail.com>
2025-01-06 10:28:06 -0700

    GPU pipeline updates for `cca_classify` (#23)
    Test suite runs to completion

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
-#BSUB -n 1
-#BSUB -J aatrnaseq 
-#BSUB -e logs/snakemake_%J.err
-#BSUB -o logs/snakemake_%J.out
 
-mkdir -p logs
+#BSUB -n 1
+#BSUB -J aatrnaseq-main
+#BSUB -e results/logs/aatrnaseq-main_%J.err
+#BSUB -o results/logs/aatrnaseq-main_%J.out
+
+mkdir -p results/logs
 
 snakemake \
   --configfile=config/config.yml \
