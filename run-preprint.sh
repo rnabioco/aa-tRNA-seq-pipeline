93f446e61cedf0bd5a455e7b24f37674c9d5841a
Jay Hesselberth <jay.hesselberth@gmail.com>
2025-01-10 15:50:21 -0700

    Re-process preprint samples (#32)

---
+++ run-preprint.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+#BSUB -n 1
+#BSUB -J aatrnaseq-main
+#BSUB -e logs/aatrnaseq-main_%J.err
+#BSUB -o logs/aatrnaseq-main_%J.out
+
+mkdir -p logs
+
+snakemake \
+    --configfile=config/config-preprint.yml \
+    --profile cluster
