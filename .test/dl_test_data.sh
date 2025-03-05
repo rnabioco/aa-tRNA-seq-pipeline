87b9877412f98e58e38e9f7d960a3b19efe05c63
Laura White <laura.white+github@gmail.com>
2025-03-05 14:46:38 -0700

    failed attempt to make, run test data locally

--- .test/dl_test_data.sh
+++ .test/dl_test_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-wget https://aatrnaseq-testdata.s3.amazonaws.com/test_data.tar.gz
+curl -L -o test_data.tar.gz https://aatrnaseq-testdata.s3.amazonaws.com/test_data.tar.gz
+
 tar -zxvf test_data.tar.gz
 rm test_data.tar.gz
